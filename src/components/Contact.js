ded114736bdc7f8b72d1e88de3463bbef0afc052
Poojitha Vasa <126378612+Chinni0404@users.noreply.github.com>
2023-10-20 11:59:33 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: poojitha.vasa.2004@gmail.com</p>
+      <p>Phone: +91 (8523087135)</p>
+    </section>
+  );
+}
+
+export default Contact;
